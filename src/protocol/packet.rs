b37ddb38d7e77f7ca7faae6353443d6df4dbc1da
yanggaofeng <yanggf23@gmail.com>
2019-11-26 16:47:53 +0800

    add parse address

--- src/protocol/packet.rs
+++ src/protocol/packet.rs
@@ -1,5 +1,7 @@
 use crate::protocol::packet::SessionStage::AuthSelect;
 use crate::protocol::packet::AuthType::{Non, Gssapi, NamePassword, IanaAssigned, Reserved, NonAccept};
+use crate::protocol::packet::CmdType::{Connect, Bind, Udp};
+use crate::protocol::packet::AddressType::{Ipv4, Domain, Ipv6};
 
 /// this packet is for authentication method
 /// selecting request when client finishes connecting.
@@ -12,12 +14,22 @@ pub struct AuthSelectRequest {
 
 pub fn parse_auth_select_request_packet(data: &[u8]) -> Result<AuthSelectRequest, &'static str> {
     let len = data.len();
+    if len < 2 {
+        return Err("data not enough.");
+    }
+
     // version
     let mut version = parse_version(data.get(0).cloned())?;
 
     // num of methods
     let n_methods = data.get(1).cloned().unwrap();
     let num = n_methods;
+
+    // verify
+    let total: usize = usize::from(2 + n_methods);
+    if data.len() != total {
+        return Err("data length not right.");
+    }
 
     let mut i = 0;
     let mut methods = Vec::<AuthType>::new();
@@ -29,12 +41,6 @@ pub fn parse_auth_select_request_packet(data: &[u8]) -> Result<AuthSelectRequest
         i = i + 1;
     }
 
-    // verify
-    let total: usize = usize::from(2 + n_methods);
-    if data.len() != total {
-        return Err("too many data.");
-    }
-
     let result = AuthSelectRequest {
         version,
         n_methods,
@@ -42,26 +48,6 @@ pub fn parse_auth_select_request_packet(data: &[u8]) -> Result<AuthSelectRequest
     };
 
     Ok(result)
-}
-
-fn parse_version(version: Option<u8>) -> Result<Version, &'static str> {
-    match version {
-        Some(5) => Ok(Version::Socks5),
-        Some(_) => Ok(Version::Others),
-        None => Err("empty version num.")
-    }
-}
-
-fn parse_auth_type(auth: Option<u8>) -> Result<AuthType, &'static str> {
-    match auth {
-        Some(0) => Ok(Non),
-        Some(1) => Ok(Gssapi),
-        Some(2) => Ok(NamePassword),
-        Some(3) => Ok(IanaAssigned),
-        Some(0x80) => Ok(Reserved),
-        Some(0xff) => Ok(NonAccept),
-        _ => return Err("auth method not supported.")
-    }
 }
 
 impl AuthSelectRequest {
@@ -101,8 +87,8 @@ pub fn parse_auth_select_reply_packet(data: &[u8]) -> Result<AuthSelectReply, &'
     Ok(result)
 }
 
-impl AuthSelectReply{
-    pub fn version(&self) -> &Version{
+impl AuthSelectReply {
+    pub fn version(&self) -> &Version {
         &self.version
     }
 
@@ -121,6 +107,58 @@ pub struct DstServiceRequest {
     port: u16,
 }
 
+pub fn parse_dst_service_request(data: &[u8]) -> Result<DstServiceRequest, &str> {
+    let len = data.len();
+    if len < 4 {
+        return Err("data not enough");
+    }
+
+    let version = parse_version(data.get(0).cloned())?;
+    let cmd = parse_cmd(data.get(1).cloned())?;
+    let reserve = match data.get(2).cloned() {
+        Some(num) => num,
+        None => 0
+    };
+
+    let addr_type = parse_address_type(data.get(3).cloned())?;
+
+    Err("err")
+}
+
+fn parse_dst_address(data: &[u8], addr_type: AddressType) -> Result<String, &str> {
+    let len = data.len();
+    match addr_type {
+        Ipv4 => {
+            if len < 4 {
+                return Err("data not enough in ipv4 type.");
+            }
+            parse_address_from_bytes(data.get(0..4).unwrap())
+        }
+        Ipv6 => {
+            if len < 16 {
+                return Err("data not enough in ipv6 type.");
+            }
+            parse_address_from_bytes(data.get(0..16).unwrap())
+        }
+        Domain => {
+            let addr_len = usize::from(data.get(0).cloned().unwrap());
+            if len < addr_len {
+                return Err("data not enough in domain type.");
+            }
+            let byte_array = data.get(1..addr_len + 1).unwrap();
+            parse_address_from_bytes(byte_array)
+        }
+    }
+}
+
+fn parse_address_from_bytes(bytes: &[u8]) -> Result<String, &str> {
+    match std::str::from_utf8(bytes) {
+        Ok(addr) => Ok(String::from(addr)),
+        Err(e) => Err("err from bytes to utf8 string.")
+    }
+}
+
+
 /// his packet is for target destination service request from server
 pub struct DstServiceReply {
     version: Version,
@@ -138,6 +176,14 @@ pub enum Version {
     Others,
 }
 
+fn parse_version(version: Option<u8>) -> Result<Version, &'static str> {
+    match version {
+        Some(5) => Ok(Version::Socks5),
+        Some(_) => Ok(Version::Others),
+        None => Err("empty version num.")
+    }
+}
+
 /// auth type enum
 #[derive(Debug, PartialEq)]
 pub enum AuthType {
@@ -149,11 +195,32 @@ pub enum AuthType {
     NonAccept,
 }
 
+fn parse_auth_type(auth: Option<u8>) -> Result<AuthType, &'static str> {
+    match auth {
+        Some(0) => Ok(Non),
+        Some(1) => Ok(Gssapi),
+        Some(2) => Ok(NamePassword),
+        Some(3) => Ok(IanaAssigned),
+        Some(0x80) => Ok(Reserved),
+        Some(0xff) => Ok(NonAccept),
+        _ => return Err("auth method not supported.")
+    }
+}
+
 /// cmd type enum
 pub enum CmdType {
     Connect,
     Bind,
     Udp,
+}
+
+fn parse_cmd(cmd: Option<u8>) -> Result<CmdType, &'static str> {
+    match cmd {
+        Some(1) => Ok(Connect),
+        Some(2) => Ok(Bind),
+        Some(3) => Ok(Udp),
+        _ => Err("cmd type not support.")
+    }
 }
 
 /// address type enum
@@ -162,6 +229,15 @@ pub enum AddressType {
     Ipv4,
     Domain,
     Ipv6,
+}
+
+fn parse_address_type(addr_type: Option<u8>) -> Result<AddressType, &'static str> {
+    match addr_type {
+        Some(1) => Ok(Ipv4),
+        Some(3) => Ok(Domain),
+        Some(4) => Ok(Ipv6),
+        _ => Err("address type not support.")
+    }
 }
 
 /// reply type enum
